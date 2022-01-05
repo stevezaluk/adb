acfc680471ecbcd62871671d6a026d1186b50fbc
Steven Zaluk <stevezaluk@gmail.com>
2022-01-05 03:16:17 -0500

    Created Device class

--- include/device.hpp
+++ include/device.hpp
@@ -1,3 +1,29 @@
 #include <iostream>
+#include <string>
+#include <libusb-1.0/libusb.h>
 
 using namespace std;
+
+class Device {
+    public:
+        string state;
+
+        libusb_device *device;
+        libusb_device_descriptor desc;
+
+        Device();
+        ~Device();
+        
+        void upload(string filename);
+        void download(string filename);
+        void install(string apk);
+        void uninstall(string appName);
+
+        void command(string command);
+        void shell();
+
+        void reboot(string mode);
+        void info();
+
+        void wipe_user_data();
+};

b10539fc68600c28e8e4c6ff32c4acba520a3124
Steven Zaluk <stevezaluk@gmail.com>
2022-01-06 02:39:35 -0500

    Created USBProtocol class for input, output, device discovery

---
+++ include/usb.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../include/device.hpp"
+#include <libusb-1.0/libusb.h>
+
+using namespace std;
+
+#ifndef USB_H
+#define USB_H
+
+class USBProtocol {
+    private:
+        int deviceCount;
+        libusb_context *context;
+        libusb_device **deviceList;
+        Device *currentDevice;
+    
+    public:
+
+        USBProtocol();
+        USBProtocol(libusb_context *context);
+        ~USBProtocol();
+
+        void findDevices();
+
+        void setCurrentDevice(libusb_device *device);
+        void setCurrentDevice(uint8_t serialNumber);
+        void setCurrentDevice(string serialNumber);
+
+        void writeData(void *data);
+        void readData();
+};
+
+#endif USB_H
---
+++ src/usb.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+
+#include "../include/usb.hpp"
+
+using namespace std;

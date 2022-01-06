33594e8a898cde69963b83c0972d94c5b3a36b04
Steven Zaluk <stevezaluk@gmail.com>
2022-01-06 03:39:48 -0500

    Defined USBProtocol class, new libusb session starts when constructor is called

--- src/usb.cpp
+++ src/usb.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <string>
 
+
+#include <libusb-1.0/libusb.h>
 #include "../include/usb.hpp"
 
 using namespace std;
+
+USBProtocol::USBProtocol() {
+    int init = libusb_init(&this->context);
+    if(init == 0) {
+        cout << "[libusb] Initialized" << endl;
+    } else {
+        cout << "[libusb] Error: Failed to initialize libusb" << endl;
+        exit(0);
+    }
+
+    this->deviceList = nullptr;
+}
+
+USBProtocol::USBProtocol(libusb_context *context) {
+    int init = libusb_init(&context);
+    if(init == 0) {
+        cout << "[libusb] Initialized" << endl;
+    } else {
+        cout << "[libusb] Error: Failed to initialize libusb" << endl;
+        exit(0);
+    }
+
+    this->deviceList = nullptr;
+}
+
+USBProtocol::~USBProtocol() {
+    libusb_free_device_list(this->deviceList, this->deviceCount);
+    libusb_exit(this->context);
+}

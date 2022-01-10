7148c5a42760c7c99d8c8025d58829946420da25
Steven Zaluk <stevezaluk@gmail.com>
2022-01-10 18:50:25 -0500

    Helper functions for validating vendorId

---
+++ include/utils.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string openVendorFile();
+bool checkForVendor(string vendorId);
---
+++ src/utils.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "../include/utils.hpp"
+
+using namespace std;
+
+string openVendorFile() {
+    string contents = "";
+    
+    ifstream file("../vendor_list.txt");
+
+    file >> contents;
+
+    file.close();
+
+    return contents;
+}
+
+bool checkForVendor(string vendorId) {
+    ifstream file("/Users/szaluk/lab/projects/adb/vendor_list.txt");
+
+    while(!file.eof()) {
+        string line;
+        string vendor;
+
+        getline(file, line);
+
+        vendor = line.substr(0, line.find('#'));
+        if(vendor == vendorId) {
+            file.close();
+            return true;
+        }
+    }
+
+    file.close();
+    return false;
+}

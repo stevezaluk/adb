3d58cff5dbf9d77f9b26104c84fed1120bdda609
Steven Zaluk <stevezaluk@gmail.com>
2022-01-10 18:43:34 -0500

    Created constructor, and getters/setters

--- src/device.cpp
+++ src/device.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
+#include <libusb-1.0/libusb.h>
+#include "../include/device.hpp"
 
 using namespace std;
+
+Device::Device() {
+    
+}
+
+Device::Device(libusb_device *device) {
+    Device::device = device;
+
+}
+
+libusb_device* Device::getDevice() {
+    return Device::device;
+}
+
+libusb_device_descriptor Device::getDeviceDesc() {
+    return this->desc;
+}
+
+libusb_device_handle* Device::getDeviceHandle() {
+    return Device::handle;
+}
+
+void Device::setDevice(libusb_device *device) {
+    Device::device = device;
+}
+
+void Device::setDeviceDesc(libusb_device_descriptor desc) {
+    this->desc = desc;
+}
+
+void Device::setDeviceHandle(libusb_device_handle *handle) {
+    Device::handle = handle;
+}

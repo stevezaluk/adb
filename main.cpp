0196c3544c240d6f1d6530000ea5f1b41c214bf8
Steven Zaluk <stevezaluk@gmail.com>
2022-01-10 18:50:50 -0500

    Created basic usage

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+#include "include/usb.hpp"
+
 using namespace std;
 
+void usage() {
+    cout << "adb - Android Debug Bridge" << endl << endl;
+    cout << "Basics: " << endl;
+    cout << "help : Print this page" << endl;
+    cout << "devices : Scan and display connected devices" << endl;
+    cout << "-s [serialNumber] : Use device with this serial number" << endl;
+}
+
 int main(int argc, char **argv) {
-    cout << "ADB";
-    
     return 0;
 }
